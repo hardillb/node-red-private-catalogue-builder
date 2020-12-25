821728914fe08612975ae5197daaf0802fd9afc5
Ben Hardill <hardillb@gmail.com>
2020-12-25 13:04:41 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# node-red-private-catalogue-builder
+
+A minimal web app to host a `catalogue.json` file for a private repository of
+Node-RED nodes.
+
+Intended to be run in it's own container as part of either 
+[multi-tenant-node-red]() or [multi-tenant-node-red-k8s]()
+
+## Configure
+
+The container takes the following Environment variables:
+
+ - PORT - Which port to listen on (defaults to `3000`)
+ - HOST - Which local IP Address to bind to (defaults to `0.0.0.0`)
+ - REGISTRY - A host and optional port number to connect to the NPM registry (defaults to `registry:4873`)
+
+ It presents 2 HTTP endpoints
+
+  - /update - a POST to this endpoint will trigger a rebuild of the catalogue
+  - /catalogue.json - a GET request returns the current catalogue
+
+The `/update` endpoint is intended to be used with the verdaccio private registry configured to send notifications
+when packages are uploaded/updated. e.g.
+
+```
+notify:
+  method: POST
+  headers: [{'Content-Type': 'application/json'}]
+  endpoint: http://catalogue/update
+  content: '{"name": "{{name}}", "versions": "{{versions}}", "dist-tags": "{{dist-tags}}"}'
+```
+
+## Deploy
+
+### Docker-compose
+
+```
+  catalogue:
+    image: catalogue
+    networks:
+      - internal
+     environment:
+      - "REGISTRY=registry:4873"
+    depends_on:
+      - registry
+    expose:
+      - "3000"
+```
+
+### Kubernetes
+
+```
+      - name: catalogue
+        image: private.example.com/catalogue
+        ports:
+        - containerPort: 3000
+        env:
+        - name: REGISTRY
+          value: 'registry:4873'
+```
---
+++ index.js
@@ -0,0 +1,93 @@
+const morgan = require('morgan')
+const express = require('express')
+const superagent = require('superagent')
+
+
+const port = (process.env.PORT || 3000)
+const listenHost = (process.env.HOST || '0.0.0.0')
+const registryHost = (process.env.REGISTRY || 'registry:4873') 
+const keyword = "node-red"
+
+const url = "http://" + registryHost +  "/-/all"
+
+const catalogue = {
+  "name":"Ben's custom catalogue",
+  "updated_at": new Date().toISOString(),
+  "modules": [
+    // {
+    //   "id": "@ben/ben-node-random",
+    //   "version": "1.0.0",
+    //   "description": "A node-red node that generates random numbers",
+    //   "keywords": [
+    //     "node-red",
+    //     "random"
+    //   ],
+    //   "updated_at": "2020-09-21T18:37:50.673Z",
+    //   "url": "http://flows.hardill.me.uk/node/ben-red-random"
+    // }
+  ]
+}
+
+function update() {
+
+	//reset list
+	catalogue.modules = [];
+
+	superagent.get(url)
+	.end((err, res) => {
+		if (!err) {
+			const nodes = res.body;
+			var nodeNames = Object.keys(nodes);
+			const index = nodeNames.indexOf("_updated");
+			if (index > -1) {
+			  nodeNames.splice(index, 1);
+			}
+
+			for (const node in nodeNames) {
+				var n = nodes[nodeNames[node]];
+				if (n.keywords.indexOf(keyword) != -1) {
+					var entry = {
+						id: n.name,
+						version: n["dist-tags"].latest,
+						description: n.description,
+						keywords: n.keywords,
+						updated_at: n.time.modified,
+						url: "http://" + registryHost + "/-/web/details/" + n.name
+					}
+					catalogue.modules.push(entry)
+				}
+			}
+
+			console.log(JSON.stringify(catalogue, null, 2));
+		} else {
+			console.log(err);
+		}
+	});
+
+}
+
+const app = express()
+app.use(morgan("combined"))
+app.use(bodyParser.json())
+
+app.post('/update', (req, res, next) => {
+	const update = req.body
+
+	update()
+	res.status(200).send();
+})
+
+app.get('/catalogue.json', (req, res, next) => {
+	res.send(catalogue)
+})
+
+// app.head('/catalogue.json', (req,res,next) => {
+	
+// })
+
+update()
+
+const server = http.Server(app);
+server.listen(port, listenHost, function(){
+	logger.info(util.format('App listening on  %s:%d!', listenHost, port));
+});
