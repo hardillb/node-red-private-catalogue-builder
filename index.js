c7c66376a6df67ccae47440db4c131d3bed08360
Ben Hardill <hardillb@gmail.com>
2023-09-29 22:50:39 +0100

    Fix details link

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function update() {
 								description: n.description,
 								keywords: n.keywords,
 								updated_at: n.time.modified,
-								url: registryHost + "/-/web/details/" + n.name
+								url: registryHost + "/-/web/detail/" + n.name
 							}
 
 							if (moduleDetails.types.length) {
